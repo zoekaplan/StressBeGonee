e6c8399ab46a5151f7db4212f163d940a4ac8c5b
zoerkaplan <zoerkaplann@gmail.com>
2017-11-01 13:53:50 -0700

    commit

--- app/src/main/java/com/example/zoerebeccakaplan/stressbegone/FeelBetter.java
+++ app/src/main/java/com/example/zoerebeccakaplan/stressbegone/FeelBetter.java
@@ -1,13 +1,35 @@
 package com.example.zoerebeccakaplan.stressbegone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class FeelBetter extends AppCompatActivity {
+public class FeelBetter extends AppCompatActivity implements View.OnClickListener {
+
+    private Button startOver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feel_better);
+
+        wireWidgets();
+        setOnCLickListeners();
+    }
+
+    private void wireWidgets() {
+        startOver = (Button) findViewById(R.id.button_start_over);
+    }
+
+    private void setOnCLickListeners() {
+        startOver.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(FeelBetter.this, MainActivity.class);
+        startActivity(i);
     }
 }
